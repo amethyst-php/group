379975fd39c22a6780f6e140bb4b34d3208e660a
railken <railken@protonmail.ch>
2019-09-09 06:58:27 +0200

    index over unique in migration schema

--- database/migrations/0000_00_00_000000_create_groups_table.php
+++ database/migrations/0000_00_00_000000_create_groups_table.php
@@ -14,7 +14,7 @@ class CreateGroupsTable extends Migration
     {
         Schema::create(Config::get('amethyst.group.data.group.table'), function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name')->index();
             $table->text('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
